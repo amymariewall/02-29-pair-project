6ca11292c244e2d04ca05ae74cbd1d3713669254
Amy Wall <amymariewall@gmail.com>
2016-03-01 13:53:36 -0600

    Login page created

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -4,3 +4,10 @@ MyApp.get "/login" do
   erb :"logins/login"
 end
 
+MyApp.post "/login/create" do 
+  @user = User.find_by_email(params[:email])
+  if @user != nil && @user.password == params[:password]
+      session["user_id"] = @user.id
+      erb :"logins/success"
+  end
+end
