f23422d2ea7bfac0cd7f098357937d65c979d8e1
Amy Wall <amymariewall@gmail.com>
2016-03-02 15:57:42 -0600

    Validation created for logins, sending login errors to the login page

--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -6,12 +6,16 @@ end
 
 MyApp.post "/login/create" do 
   @user = User.find_by_email(params[:email])
-  if @user != nil && @user.password == params[:password]
+    if @user != nil && @user.password == params[:password]
       session["user_id"] = @user.id    
       redirect "/user/profile/#{@user.id}"
-    else
-      redirect "/login"
-  end
+    elsif @user != nil && @user.password != params[:password]
+      @error = "Invalid password."
+      erb :"logins/login"
+    elsif @user == nil
+      @error = "No user for that email address."  
+      erb :"logins/login"
+    end
 end
 
 MyApp.get "/logout" do 
