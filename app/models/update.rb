b3f5139600a98b6761db27c6c492cb7413de6ca4
Alexandra Millatmal <alexandrajmh@gmail.com>
2016-04-28 14:36:22 -0500

    moved display logic into a method in the Update class/Model. Next step: add complexity to the logic so that times display differently

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,4 +1,10 @@
 #this sets active record to our Update class. Columns are "user_id" and "status_update".
 
 class Update < ActiveRecord::Base
+  def time_display
+    return (Time.now - self.created_at.to_time).to_i/60 
+  end
+
+  # @update = Update.where...
+  # @update.time_display
 end
