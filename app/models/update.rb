d7e93e3a0f582b2bcc2c5dd5a94b3fcd06d4d541
Tyler <tfisher322@gmail.com>
2016-04-30 01:35:22 -0500

    When the status is under two hours but at or over an hour old it will now appear as about one hour ago.

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,27 +3,27 @@
 class Update < ActiveRecord::Base
 
   def display_minute(min_ago)
-    min_ago.to_s + " minute ago"
+    min_ago.to_s + " minute ago "
   end
 
   def display_minutes(min_ago)
-    min_ago.to_s + " minutes ago"
+    min_ago.to_s + " minutes ago "
   end
 
   def display_hour(min_ago)
-    min_ago.to_s + " about one hour ago"
+    "About one hour ago "
   end
 
   def display_hours(min_ago)
-    (min_ago/60).to_s + " hours ago"
+    (min_ago/60).to_s + " hours ago "
   end
 
   def display_day
-    "one day ago"
+    "One day ago "
   end
 
   def display_days(min_ago)
-    (min_ago/1440).to_s + " days ago"
+    (min_ago/1440).to_s + " days ago "
   end
 
   def posted_ago
@@ -32,9 +32,9 @@ class Update < ActiveRecord::Base
       return self.display_minutes(min_ago)
     elsif min_ago == 1
       return self.display_minute(min_ago)
-    elsif min_ago == 60
+    elsif min_ago.floor < 120
       return self.display_hour(min_ago)
-    elsif min_ago > 60 && min_ago < 1440
+    elsif min_ago > 120 && min_ago < 1440
       return self.display_hours(min_ago)
     else
       if (min_ago/1440).floor == 1
